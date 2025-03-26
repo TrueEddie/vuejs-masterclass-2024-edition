21a38bf3d382e851a03ac932f7acfa8b346010ef
Eddie Cosme <ecosme@me.com>
2025-03-25 22:51:16 -0400

    Commenting out code that was causing es lint error

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
-import { ref } from "vue";
+// import { ref } from "vue";
 
-const courseName = ref("Vue.js Masterclass");
+// const courseName = ref("Vue.js Masterclass");
 </script>
 
 <template>
